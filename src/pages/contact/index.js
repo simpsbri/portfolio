bc86a67f3726e86a7df575bd33d5b28114616e06
simpsbri <simpsbri@gmail.com>
2024-04-06 20:33:26 -0500

    update to contact info

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -31,7 +31,7 @@ export const ContactUs = () => {
               </a>
               <br />
               <br />
-              {contactConfig.hasOwnProperty('YOUR_FONE') ? (
+              {contactConfig.hasOwnProperty('YOUR_PHONE') ? (
                 <p>
                   <strong>Phone:</strong> {contactConfig.PHONE}
                 </p>
